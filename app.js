01a9f0c9290b9e09f0a1c3894cd494ebe5bbfc2c
kateah1 <kate.hurd64@gmail.com>
2016-06-13 12:57:41 -0400

    1257

---
+++ app.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+	if (num % 3 && num % 5 == 0) {
+		console.log(FizzBuzz)
+	}
+	if else(num % 3 == 0) {
+		console.log(Fizz)
+	}
+	if else(num % 5 == 0) {
+		console.log(Buzz)
+	}
+	else() {
+		console.log(num)
+	}
+
+
+
+
+
+});
